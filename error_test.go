0de92d6adda55728ec1803973b398e67f14c2362
Dima Medvedev <d@medvedev.io>
2021-01-05 10:15:41 +0000

    Add JSON-mashalable error type

---
+++ error_test.go
@@ -0,0 +1,48 @@
+package xjson
+
+import (
+	"encoding/json"
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type tmpError struct {
+	Error Error
+}
+
+func TestErrorUnmarshal(t *testing.T) {
+	data := []byte(`{
+    "Error": "example error message"
+}`)
+	var j tmpError
+	err := json.Unmarshal(data, &j)
+	require.NoError(t, err)
+	want := tmpError{Error: Error{Err: errors.New("example error message")}}
+
+	assert.Equal(t, want, j)
+}
+
+func TestErrorUnmarshalEmptyString(t *testing.T) {
+	data := []byte(`{
+    "Error": ""
+}`)
+	var j tmpError
+	err := json.Unmarshal(data, &j)
+	require.Error(t, err)
+}
+
+func TestErrorMarshal(t *testing.T) {
+	j := tmpError{
+		Error: Error{
+			Err: errors.New("example error message"),
+		},
+	}
+
+	want := []byte(`{"Error":"example error message"}`)
+	b, err := json.Marshal(&j)
+	require.NoError(t, err)
+	assert.Equal(t, want, b)
+}
